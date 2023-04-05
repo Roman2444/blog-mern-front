aab76b2d86f4ba1b87eefff4938a7e6ccebb3f35
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-04-05 12:51:01 +0300

    add fetchTags

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -6,6 +6,11 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   return data;
 });
 
+export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
+  const { data } = await axios.get("tags");
+  return data;
+});
+
 const initialState = {
   posts: {
     item: [],
@@ -33,6 +38,18 @@ const postsSlice = createSlice({
     [fetchPosts.rejected]: (state) => {
       state.posts.item = [];
       state.posts.status = "error";
+    },
+    [fetchTags.pending]: (state) => {
+      state.tags.item = [];
+      state.tags.status = "loading";
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+      state.tags.item = action.payload;
+      state.tags.status = "loaded";
+    },
+    [fetchTags.rejected]: (state) => {
+      state.tags.item = [];
+      state.tags.status = "error";
     },
   },
 });
