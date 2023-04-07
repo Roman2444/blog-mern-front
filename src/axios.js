fced822136d16b32af6c1accda15116fb17cd7df
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-04-07 11:42:29 +0300

    added middleWare Autorisation to axios

--- src/axios.js
+++ src/axios.js
@@ -4,4 +4,12 @@ const instance = axios.create({
   baseURL: "http://localhost:4444",
 });
 
+// добавляем мидлвэ, чтобы при каждом запросе на бэкэнд отравлялся токен из localStorage,
+// чтобы понимать авторизован пользователь или нет
+
+instance.interceptors.request.use((config) => {
+  config.headers.Authorisation = window.localStorage.getItem("token");
+  return config;
+});
+
 export default instance;
