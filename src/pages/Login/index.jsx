9c73bc661a7fb4b20a7845a5f130432ace23e095
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-04-07 11:33:18 +0300

    saved token in localStorage

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -27,14 +27,21 @@ export const Login = () => {
     mode: "onChange",
   });
 
-  const onSubmit = (values) => {
-    dispatch(fetchAuth(values));
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      alert("Ошибка аторизации");
+    }
+
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
   };
 
   if (isAuth) {
     return <Navigate to="/" />;
   }
-  console.log(isAuth);
 
   return (
     <Paper classes={{ root: styles.root }}>
